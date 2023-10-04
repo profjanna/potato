daeb7db25e0ab47c8a443efec68a48587462ba06
profjanna <146989645+profjanna@users.noreply.github.com>
2023-10-04 14:47:23 -0400

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,14 @@
-let mic;
+let potato;
+
+function preload(){
+  potato = loadImage("potato1.jpg");
+}
 
 function setup() {
-  createCanvas(900, 900);
-  mic = new p5.AudioIn();
-  mic.start();
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(255);
-  micLevel = 1000*(mic.getLevel());
-  textSize(micLevel);
-  //text('move your mouse',20,200);
-  fill(micLevel,0,micLevel,90);
-  text('ha',200,200);
-  if(mouseX>200&&mouseY>200 ){
-      
-      if (mouseX<300 && mouseY<300)          {
-    background(255);    
-    textSize(200);
-    text('hi',200,200);
-          }
-     }
-console.log(micLevel);
-}
-
-function mouseClicked(){
-  
+  background(220);
+  image(potato,0,0);
 }
